d54d07c494baf360a86c13521f2a788264fc6178
Lksmv <lks.migueel@gmail.com>
2025-11-06 04:04:04 -0300

    fix

--- set-env.js
+++ set-env.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const backendUrl = process.env.BACKEND_API_URL || process.env.BACKEND_URL || 'http://localhost:8080';
+const backendUrl = process.env.BACKEND_API_URL || process.env.BACKEND_URL || 'https://loja-jogo.onrender.com';
 
 const targetDir = path.join(__dirname, 'src', 'environments');
 if (!fs.existsSync(targetDir)) fs.mkdirSync(targetDir, { recursive: true });
